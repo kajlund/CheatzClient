084026bfb96d264718520f4c92be6a52535e1dc2
Kaj Lund <kaj.lund@gmail.com>
2023-01-19 20:03:21 +0200

    Cleaned up burger button

--- src/components/Burger.svelte
+++ src/components/Burger.svelte
@@ -1,27 +1,29 @@
 <script>
-  export let open;
+  export let open ;
 
-  const toggle = () => {
-    open = !open;
-  };
+  const toggle = () => open = !open;
 </script>
 
 <button class:open on:click={toggle}>
   <!-- svg with 3 lines -->
   <svg width="32" height="24">
-    <line id="top" x1="0" y1="2" x2="32" y2="2" />
-    <line id="middle" x1="0" y1="12" x2="24" y2="12" />
-    <line id="bottom" x1="0" y1="22" x2="32" y2="22" />
+    <line id="top" x1="2" y1="4" x2="30" y2="4" />
+    <line id="middle" x1="2" y1="13" x2="24" y2="13" />
+    <line id="bottom" x1="2" y1="22" x2="30" y2="22" />
   </svg>
 </button>
 
 <style>
   button {
-    border: none;
+    background-color: transparent;
+    border: 2px solid #666;
+    border-radius: 6px;
+    width: 46px;
+    height: 33px;
   }
   svg {
     min-height: 24px;
-    transition: transform 0.3s ease-in-out;
+    transition: transform 0.2s ease-in-out;
   }
   svg line {
     stroke: #666; /* `currentColor` means inherit color from the text color */
